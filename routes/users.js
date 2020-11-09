b06b05ad0ef0cd2ce6181bfe42f375aba361c9ca
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2020-11-09 15:08:53 +0530

    fixed issue in face login api

--- routes/users.js
+++ routes/users.js
@@ -703,4 +703,25 @@ try{
 
 });
 
+router.get("/getstudentuname/:studentname", async (req, res) => {
+    try {
+        const id = req.params.studentname;
+        const user = await User.findOne({ studentname: id });
+
+        if (user) {
+            var student = ({
+                studentname : user.studentname,
+                username : user.username
+            })
+
+            res.status(200).json(student); 
+    
+        } else {
+            res.status(404).json({ message: "No valid entry found" });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
+});
+
 module.exports = router;
